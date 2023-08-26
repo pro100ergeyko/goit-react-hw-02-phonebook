6bad7ecf400fbfd9ac7cdc34c9181cffc23aeec6
pro100ergeyko <124717620+pro100ergeyko@users.noreply.github.com>
2023-08-26 10:28:14 +0300

    add base task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Formik, Field, Form } from 'formik';
+import { nanoid } from 'nanoid';
+import * as Yup from 'yup';
+
+import { Container } from './Container.styled';
+
+const PhoneBookSchema = Yup.object().shape({
+  Name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required(
+      'Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d Artagnan'
+    ),
+  number: Yup.number()
+    .min(7, 'Too Short!')
+    .max(9, 'Too Long!')
+    .required(
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    ),
+});
+
+export class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Formik
+          initialValues={{ contacts: [], filter: '', name: '', number: '' }}
+          validationSchema={PhoneBookSchema}
+          onSubmit={values => {
+            console.log(values);
+          }}
+        >
+          <Form>
+            <label>
+              Name
+              <Field name="name" type="text" />
+            </label>
+            <label>
+              Number
+              <Field name="number" type="number" />
+            </label>
+
+            <button type="submit">Add contact</button>
+          </Form>
+        </Formik>
+      </Container>
+    );
+  }
+}
